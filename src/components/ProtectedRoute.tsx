2fcf440342316d0db9fc241b825cfabec3ce5995
LkiZt <d.sinyakin@mail.ru>
2025-04-04 21:31:16 +0300

    123
    123

--- src/components/ProtectedRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-export const ProtectedRoute = () => {
-  const isAuthenticated = localStorage.getItem('authToken') !== null;
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-};
