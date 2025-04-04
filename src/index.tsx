c596d9622db3a5af596b133b0e1c1100ac0c6989
LkiZt <d.sinyakin@mail.ru>
2025-04-04 21:26:31 +0300

    11
    11

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './styles/App.css';
+import RegistrationForm from './components/RegistrationForm';
+import LoginPage from './components/LoginPage';
+import HomePage from './components/HomePage';
+import Zaglushka from './components/Zaglushka';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
+const App: React.FC = () => {
+    return (
+        <Routes>
+            {/* Открытые маршруты (доступны без авторизации) */}
+            <Route path="/login" element={<LoginPage />} />
+
+            {/* Защищённые маршруты (только для авторизованных) */}
+            <Route element={<ProtectedRoute />}>
+            <Route path="/register" element={<RegistrationForm />} />
+            <Route path="/home" element={<Zaglushka />} />
+            {/* <Route path="/home2" element={<HomePage />} /> */}
+            </Route>
+        </Routes>
+    );
+};
+
+export default App;
